26df85710221ccf2adc80574898755dbbb009085
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-08-03 21:40:45 +0600

    some changes

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ humClose.addEventListener("click", function () {
 //     mainMenu.style.left = "-100%";
 // });
 //
+
+
+function loadResources() {
+    if (window.innerWidth > 768) { // Adjust the width as per your requirement
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'https://cdn.jsdelivr.net/gh/mdabdulkyum1/c-a@main/style.css';
+        document.head.appendChild(link);
+
+        var script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/gh/mdabdulkyum1/c-a@main/main.js';
+        document.body.appendChild(script);
+    }
+}
+
+loadResources();
+
+window.addEventListener('resize', function() {
+    // Reload the page on resize if necessary to re-evaluate the condition
+    location.reload();
+});
